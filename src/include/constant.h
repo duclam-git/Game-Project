61c347507839dc76b37e3077525d610e42ae5d67
duclam-git <24020194@vnu.edu.vn>
2025-03-30 21:19:49 +0700

    more fixing

---
+++ src/include/constant.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int SPAWN_SAFE_RADIUS = 100;
+
+const int PLAYER_START_HEALTH = 100;
+const int PLAYER_START_SPEED = 5;
+const int PLAYER_START_DAMAGE = 10;
+
+const int COIN_VALUE = 5;
+const int HEALTH_PACK_AMOUNT = 20;
+const int DAMAGE_UPGRADE_AMOUNT = 2;
+const int SPEED_UPGRADE_AMOUNT = 1;
+
+const int SHOTGUN_SPREAD_ANGLE = 20;
+const int SHOTGUN_BULLET_COUNT = 3;
+
+#endif
